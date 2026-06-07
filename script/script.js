2a5ea898eef3e979b97eadd937d4d6fcf3a99dd0
JVMDESIGNRD <jvaleriomota@gmail.com>
2026-06-06 23:12:17 -0400

    Refactor toppers data and modal functionality

--- script/script.js
+++ script/script.js
@@ -71,8 +71,7 @@ const toppersData = [
 
   {
     id: 9,
-    nombre:
-      "🌸 Personalizamos su topper con sus flores favoritas🌸",
+    nombre:"🌸 Personalizamos su topper con sus flores favoritas🌸",
     img: "img/wendotopper.jpeg", // topper similar
     descripcion:
       "topper para los amantes a las flores con diseño brillante y mágico, ideal para fiestas temáticas.",
